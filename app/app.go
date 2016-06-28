4d093792e0f7bf01586d7a5a7802914cc4ba6749
Adam Worrall <adam@worrall.cc>
2016-06-28 12:51:08 -0700

    A few app-wide helpers

---
+++ app/app.go
@@ -0,0 +1,19 @@
+package complaints
+
+import(
+	"net/http"
+	"time"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+	"golang.org/x/net/context"
+)
+
+func req2ctx(r *http.Request) context.Context {
+	ctx,_ := context.WithTimeout(appengine.NewContext(r), 55 * time.Second)
+	return ctx
+}
+
+func req2client(r *http.Request) *http.Client {
+	return urlfetch.Client(req2ctx(r))
+}
