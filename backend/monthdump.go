346267bcf764f167c74269f77ab35279eafdb104
Adam Worrall <adam@worrall.cc>
2017-04-04 11:24:22 -0700

    Factor out the CSV writing to aid testing (a little)

--- backend/monthdump.go
+++ backend/monthdump.go
@@ -81,6 +81,7 @@ func generateMonthlyCSV(cdb complaintdb.ComplaintDB, month,year int) (string, in
 	if err != nil {
 		return gcsName,0,err
 	}
+
 	csvWriter := csv.NewWriter(gcsHandle.IOWriter())
 
 	cols := []string{
@@ -137,6 +138,66 @@ func generateMonthlyCSV(cdb complaintdb.ComplaintDB, month,year int) (string, in
 		}
 	}
 	csvWriter.Flush()
+
+	if err := gcsHandle.Close(); err != nil {
+		return gcsName,0,err
+	}
+
+	log.Infof(ctx, "monthly CSV successfully written to %s, %d rows", gcsName, n)
+
+	return gcsName,n,nil
+}
+
+// }}}
+// {{{ generateMonthlyCSV2
+
+// Dumps the monthly CSV file into Google Cloud Storage, ready to be emailed to BKSV et al.
+
+// http://backend-dot-serfr0-1000.appspot.com/backend/monthdump?year=2016&month=4
+
+func generateMonthlyCSV2(cdb complaintdb.ComplaintDB, month,year int) (string, int, error) {
+	ctx := cdb.Ctx()
+	bucketname := "serfr0-reports"
+	
+	now := date.NowInPdt()
+	s := time.Date(int(year), time.Month(month), 1, 0,0,0,0, now.Location())
+	e := s.AddDate(0,1,0).Add(-1 * time.Second)
+	log.Infof(ctx, "Starting /be/month: %s", s)
+
+	// One time, at 00:00, for each day of the given month
+	days := date.IntermediateMidnights(s.Add(-1 * time.Second),e)
+
+	filename := s.Format("complaints-20060102") + e.Format("-20060102.csv")
+
+	gcsName := "gs://"+bucketname+"/"+filename
+	
+	if exists,err := gcs.Exists(ctx, bucketname, filename); err != nil {
+		return gcsName,0,fmt.Errorf("gcs.Exists=%v for gs://%s/%s (err=%v)", exists, bucketname, filename, err)
+	} else if exists {
+		return gcsName,0,nil
+	}
+
+	gcsHandle,err := gcs.OpenRW(ctx, bucketname, filename, "text/plain")
+	if err != nil {
+		return gcsName,0,err
+	}
+
+	tStart := time.Now()
+	n := 0
+	
+	for _,dayStart := range days {
+		dayEnd := dayStart.AddDate(0,0,1).Add(-1 * time.Second)
+		q := cdb.NewComplaintQuery().ByTimespan(dayStart, dayEnd)
+		log.Infof(ctx, " /be/month: %s - %s", dayStart, dayEnd)
+
+		tIter := time.Now()
+		if num,err := cdb.WriteCQueryToCSV(q, gcsHandle.IOWriter(), (n==0)); err != nil {
+			return gcsName,0,fmt.Errorf("failed; time since start: %s. Err: %v", time.Since(tStart),
+				iter.Err())
+		} else {
+			n += num
+		}
+	}
 
 	if err := gcsHandle.Close(); err != nil {
 		return gcsName,0,err
---
+++ complaintdb/csv.go
@@ -0,0 +1,70 @@
+package complaintdb
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"time"
+)
+
+// {{{ WriteCQueryToCSV
+
+func (cdb ComplaintDB)WriteCQueryToCSV(cq *CQuery, w io.Writer, headers bool) (int, error) {
+	csvWriter := csv.NewWriter(w)
+
+	if headers {
+		cols := []string{
+			"CallerCode", "Name", "Address", "Zip", "Email", "HomeLat", "HomeLong", 
+			"UnixEpoch", "Date", "Time(PDT)", "Notes", "ActivityDisturbed", "Flightnumber",
+			"Notes",
+			// Column names above are incorrect, but BKSV are used to them.
+			//
+			//"CallerCode", "Name", "Address", "Zip", "Email", "HomeLat", "HomeLong", 
+			//"UnixEpoch", "Date", "Time(PDT)", "Notes", "Flightnumber",
+			//"ActivityDisturbed", "CcSFO",
+		}
+		csvWriter.Write(cols)
+	}
+
+	tIter := time.Now()
+	iter := cdb.NewComplaintIterator(cq)
+
+	n := 0
+	for iter.Iterate(cdb.Ctx()) {
+		c := iter.Complaint()
+		
+		r := []string{
+			c.Profile.CallerCode,
+			c.Profile.FullName,
+			c.Profile.Address,
+			c.Profile.StructuredAddress.Zip,
+			c.Profile.EmailAddress,
+			fmt.Sprintf("%.4f",c.Profile.Lat),
+			fmt.Sprintf("%.4f",c.Profile.Long),
+
+			fmt.Sprintf("%d", c.Timestamp.UTC().Unix()),
+			c.Timestamp.Format("2006/01/02"),
+			c.Timestamp.Format("15:04:05"),
+			c.Description,
+			c.AircraftOverhead.FlightNumber,
+			c.Activity,
+			fmt.Sprintf("%v",c.Profile.CcSfo),
+		}
+
+		if err := csvWriter.Write(r); err != nil {
+			return 0,err
+		}
+
+		n++
+	}
+	if iter.Err() != nil {
+		return n,fmt.Errorf("iterator failed after %s: %v", iter.Err(), time.Since(tIter))
+	}
+
+	csvWriter.Flush()
+
+	return n,nil
+}
+
+// }}}
+
